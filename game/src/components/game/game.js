1d279c9e9e6d83761fdae686cdd10fbbc973ade9
Ryan Temple <temple.design.me@gmail.com>
2018-04-21 00:25:51 -0400

    migrated game to react

---
+++ game/src/components/game/game.js
@@ -0,0 +1,14 @@
+//Core
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Game</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
---
+++ game/src/index.js
@@ -0,0 +1,13 @@
+//Core
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import registerServiceWorker from './registerServiceWorker';
+
+//Components
+import Game from './components/game/game';
+
+//CSS
+import './index.css';
+
+ReactDOM.render(<Game/>, document.getElementById('root'));
+//registerServiceWorker();
